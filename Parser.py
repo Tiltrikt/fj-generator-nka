510f885fbe8772beda05398ff9deb5d005c3194c
tiltrikt <vladoss339@gmail.com>
2025-03-31 14:34:55 +0200

    feat: tree generation

--- Parser.py
+++ Parser.py
@@ -1,5 +1,16 @@
-import logging
 from Lexer import TokenType
+from PrettyPrint import PrettyPrintTree
+
+class Tree:
+    def __init__(self, value):
+        self.val = value
+        self.children = []
+
+    def add_child(self, child):
+        if isinstance(child, str):
+            child = Tree(child)
+        self.children.append(child)
+        return child
 
 class StateNode:
     def __init__(self):
@@ -14,7 +25,6 @@ class StateNode:
     def __str__(self):
         return f"StateNode(id={self.id}, transitions={self.transitions})"
 
-
 class NKA:
     def __init__(self):
         self.start_state = None
@@ -26,76 +36,75 @@ class NKA:
     def __str__(self):
         return f"NKA(start_state={self.start_state}, accepted_states={self.accepted_states})"
 
-
 class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
 
-    def consume(self, expected_token_type):
+    def consume(self, expected_token_type, tree):
         if self.current_token.type == expected_token_type:
+            tree.add_child(str(self.current_token))
             self.current_token = self.lexer.get_next_token()
         else:
             raise SyntaxError(f"Syntaktická chyba: očakávaný token {expected_token_type}, "
                                f"získaný {self.current_token.type}")
 
     def regular(self) -> NKA:
-        result = self.alternative()
+        pt = PrettyPrintTree(lambda x: x.children, lambda x: x.val)
+        tree = Tree("regular")
+        result = self.alternative(tree)
+        pt(tree)
         return result
 
-    def alternative(self) -> NKA:
-        result = self.sequence()
+    def alternative(self, tree) -> NKA:
+        node = tree.add_child("alternative")
+        result = self.sequence(node)
         while self.current_token.type == TokenType.PIPE:
-            self.consume(TokenType.PIPE)
+            self.consume(TokenType.PIPE, node)
             left_nka = result
-            right_nka = self.sequence()
+            right_nka = self.sequence(node)
 
             new_start = StateNode()
             alternative_nka = NKA()
-
             alternative_nka.start_state = new_start
             new_start.add_transition('', left_nka.start_state)
             new_start.add_transition('', right_nka.start_state)
             alternative_nka.accepted_states = left_nka.accepted_states.union(right_nka.accepted_states)
             result = alternative_nka
-
         return result
 
-    def sequence(self) -> NKA:
-        result = self.element()
+    def sequence(self, tree) -> NKA:
+        node = tree.add_child("sequence")
+        result = self.element(node)
         while self.current_token.type in (TokenType.SYMBOL, TokenType.LPAREN, TokenType.LCBRA, TokenType.LBRACK):
             left_nka = result
-            right_nka = self.element()
-
+            right_nka = self.element(node)
             for accept_state in left_nka.accepted_states:
                 accept_state.add_transition('', right_nka.start_state)
-
             left_nka.accepted_states = right_nka.accepted_states
             result = left_nka
         return result
 
-    def element(self) -> NKA:
+    def element(self, tree) -> NKA:
+        node = tree.add_child("element")
         if self.current_token.type == TokenType.SYMBOL:
             symbol = self.current_token.attribute
-
             start_node = StateNode()
             accept_node = StateNode()
             nka = NKA()
             start_node.add_transition(symbol, accept_node)
             nka.start_state = start_node
             nka.accepted_states.add(accept_node)
-
-            self.consume(TokenType.SYMBOL)
+            self.consume(TokenType.SYMBOL, node)
         elif self.current_token.type == TokenType.LPAREN:
-            self.consume(TokenType.LPAREN)
-            nka = self.alternative()
-            self.consume(TokenType.RPAREN)
+            self.consume(TokenType.LPAREN, node)
+            nka = self.alternative(node)
+            self.consume(TokenType.RPAREN, node)
         elif self.current_token.type == TokenType.LCBRA:
-            self.consume(TokenType.LCBRA)
-
+            self.consume(TokenType.LCBRA, node)
             start_node = StateNode()
             transitive_nka = NKA()
-            nka = self.alternative()
+            nka = self.alternative(node)
             start_node.add_transition('', nka.start_state)
             transitive_nka.accepted_states = nka.accepted_states.copy()
             transitive_nka.accepted_states.add(start_node)
@@ -103,24 +112,20 @@ class Parser:
                 accept_state.add_transition('', start_node)
             transitive_nka.start_state = start_node
             nka = transitive_nka
-
-            self.consume(TokenType.RCBRA)
+            self.consume(TokenType.RCBRA, node)
         elif self.current_token.type == TokenType.LBRACK:
-            self.consume(TokenType.LBRACK)
-
+            self.consume(TokenType.LBRACK, node)
             start_node = StateNode()
             additional_node = StateNode()
             optional_nka = NKA()
-            nka = self.alternative()
+            nka = self.alternative(node)
             start_node.add_transition('', nka.start_state)
             start_node.add_transition('', additional_node)
             optional_nka.accepted_states = nka.accepted_states.copy()
             optional_nka.accepted_states.add(additional_node)
             optional_nka.start_state = start_node
             nka = optional_nka
-
-            self.consume(TokenType.RBRACK)
-
+            self.consume(TokenType.RBRACK, node)
         return nka
 
     def parse(self) -> NKA:
